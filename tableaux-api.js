b0d148b3908e7d79644787ff9d9b77fe9f82538a
Grégoire Petitalot <perso@gregoirep.com>
2020-05-22 17:39:08 +0200

    add api table

---
+++ tableaux-api.js
@@ -0,0 +1,72 @@
+var evenement = {
+    id: 21,
+    titre: "",
+    lieu: "",
+    date: "2020-02-12 12:12:20",
+    publications: [
+        {
+            photoURL: "",
+            utilisateur: {
+                nom: "",
+            },
+        },
+        {
+            photoURL: "",
+            utilisateur: {
+                nom: "",
+            },
+        },
+    ]
+};
+
+var evenements = [
+    evenement, evenement, evenement
+];
+
+var publication = {
+
+    utilisateur: {
+        nom: "",
+        photoURL: ""
+    },
+    url: "",
+    reactions: {},
+    commentaires: [
+        {
+            id: 23,
+            date: "2020-02-12 12:12:20",
+            contenu: "",
+            utilisateur: {
+                nom: "",
+                photoURL: ""
+            },
+        },
+        {
+            id: 23,
+            date: "2020-02-12 12:12:20",
+            contenu: "",
+            utilisateur: {
+                nom: "",
+                photoURL: ""
+            },
+        },
+    ]
+};
+
+// ------------- ajout d'elements ------------------
+
+var commentaire = {
+    date: "2020-02-12 12:12:20",
+    contenu: "",
+    utilisateur: {
+        nom: "",
+        photoURL: ""
+    }
+};
+
+var evenement = {
+    id: 21,
+    titre: "",
+    lieu: "",
+    date: "2020-02-12 12:12:20"
+};
