9863434602f6d48bf66d6d95f1c73ff6505bcee9
Justine <jjuarezbellais@iut.univ-paris8.fr>
2020-05-27 16:17:28 +0200

    API : Ajout PUBLICATION/ get-by-groupe et get

---
+++ api/publication/get.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once '../../includes.php';
+
+use Publication\Publication;
+use Sanitize\Sanitize;
+
+header('Content-Type: application/json; charset=UTF-8');
+
+// Check if params are sended
+if (Sanitize::checkEmptyFields($_GET, ['id'])) {
+
+$secured = Sanitize::arrayFields($_GET, ['id']);
+
+// --------------- PROCESSING THE REQUEST------------------------
+
+$publication = new publication($db);
+$publication->id = $secured['id'];
+
+if ($publication->update($secured['id'])) {//action a faire
+
+http_response_code(200);//envoie reponse
+echo json_encode(
+[
+'id' => $publication->id,
+'description' => $publication->description,
+'photoURL',
+'date' => $publication->date,
+'utilisateur' => ['photoURL', 'nom']
+]
+);
+
+} else {
+http_response_code(500);
+echo json_encode('Server Error');
+}
+
+// --------------- END - PROCESSING THE REQUEST------------------------
+
+} else {
+
+http_response_code(400);
+echo json_encode('Missing Arguments');
+
+/* http_response_code(401);
+echo json_encode('Please login to use our services'); */
+
+}
